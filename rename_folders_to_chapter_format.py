15d6524b383b6d0a07ceefeb3f71c3c9b3420ab8
Carlos Martinez <curlosmart@gmail.com>
2024-12-28 12:48:53 -0500

    Added comments to "rename_folders_to_chapter_format".

--- rename_folders_to_chapter_format.py
+++ rename_folders_to_chapter_format.py
@@ -3,11 +3,20 @@ import re
 
 
 def rename_folders_to_chapter_format(directory):
-    for folder_name in os.listdir(directory):
+    """
+    Goes through a directory of manga chapter folders and renames the folder names to in the format "xxxx_Chapter.xxx" so that the chapter folders are in sorted order by default.
+    """
+    folder_name_list = os.listdir(directory)
+
+    for folder_name in folder_name_list:
         folder_path = os.path.join(directory, folder_name)
-        if os.path.isdir(folder_path):
+        folder_exists = os.path.isdir(folder_path)
+
+        if folder_exists:
             # Extract "Ch. xxx" using regex
-            match = re.search(r"Ch\. (\d+)", folder_name, re.IGNORECASE)
+            match = re.search(r"\bCh\. \d+\b", folder_name, re.IGNORECASE)
+
+            # If we find the Chapter Number, then rename the folder in the format of "xxxx_Chapter.xxx" using the chapter number.
             if match:
                 chapter_number = int(match.group(1))
                 new_folder_name = f"{chapter_number:04d}_Chapter.{chapter_number}"
