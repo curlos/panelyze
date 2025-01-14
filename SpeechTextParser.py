5ccfd231099573d6b47a63075633a34d28427f41
Carlos Martinez <curlosmart@gmail.com>
2025-01-14 16:40:16 -0500

    Add "calculate_reading_time" that will calculate the time required to read text based on WPM.

--- SpeechTextParser.py
+++ SpeechTextParser.py
@@ -9,8 +9,18 @@ class SpeechTextParser:
     def __init__(self):
         self.magi = Magi()
 
-    def analyze_wpm(self):
-        print("Hello World")
+    def calculate_reading_time(self, text_list, wpm=250):
+        """Calculate the time required to read the text based on WPM."""
+        # Join the list of strings into a single text
+        full_text = " ".join(text_list)
+
+        # Count the total number of words
+        word_count = len(full_text.split())
+
+        # Calculate reading time in seconds (WPM -> Words Per Second)
+        reading_time_seconds = word_count / (wpm / 60)
+
+        return reading_time_seconds
 
 
 is_running_as_main_program = __name__ == "__main__"
@@ -32,10 +42,6 @@ if is_running_as_main_program:
         magi_data_from_images = speech_text_parser.magi.get_data_from_images("wpm-test")
 
         for magi_image_data in magi_data_from_images:
-            print(magi_image_data)
-
-            breakpoint()
-
             # "is_essential_text" from what I've seen so far with one panel will tell you what the "essential" text on the page is. For example, in "panel_40.png", the essential text are the text bubbles with the conversation between Doflamingo and the World Govt. official. However, on the page, there is a "World Govt" label in the background NOT in a speech bubble. This was deemed as NOT essential by Magi. This is very useful as I only want to take into account speech or thought bubbles - not background text for both the WPM and Text-To-Speech calculations.
             # "is_essential_text" will return an array of boolean values (such as [True, True, False, False, True])
             is_essential_text_arr = magi_image_data["is_essential_text"]
@@ -54,7 +60,6 @@ if is_running_as_main_program:
                 ]
 
     # extract_text_with_pytesseract()
-
     # extract_text_with_magi()
 
     essential_text = [
@@ -62,3 +67,7 @@ if is_running_as_main_program:
         "Well... unless he managed to resurrect himself as a zombie, of course... Fuffuffuffuffuffuffuffuffuffuffuffuffuffuffuffu! Hey, it serves him right.",
         "And you call this doing your job, do you...?!!!",
     ]
+
+    wpm = 250  # Adjust WPM as needed
+    time_required = speech_text_parser.calculate_reading_time(essential_text, wpm)
+    print(f"Estimated reading time: {time_required:.2f} seconds")
