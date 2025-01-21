c21abdaa1b4f0263a6417d139c17c44b43bce48e
Carlos Martinez <curlosmart@gmail.com>
2025-01-21 15:36:16 -0500

    Add "get_emotions_from_text_matrix" to get emotions from a text matrix and map it into objects with the text and the emotion and confidence of that text.

--- EmotionAnalyzer.py
+++ EmotionAnalyzer.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from transformers import pipeline
 
 
@@ -16,13 +17,47 @@ class EmotionAnalyzer:
         result = self.emotion_pipeline(text)
         return result[0]["label"], result[0]["score"]
 
+    def get_emotions_from_text_matrix(self, text_matrix):
+        text_obj_matrix = []
+
+        for text_arr in text_matrix:
+            new_text_obj_arr = []
+
+            for text in text_arr:
+                result = self.get_emotion(text)
+                emotion, confidence = result
+
+                new_text_obj_arr.append(
+                    {"text": text, "emotion": emotion, "confidence": confidence}
+                )
+
+            text_obj_matrix.append(new_text_obj_arr)
+
+        return text_obj_matrix
+
 
 is_running_as_main_program = __name__ == "__main__"
 
 if is_running_as_main_program:
     emotion_analyzer = EmotionAnalyzer()
 
-    # Example usage
-    text = "I am absolutely thrilled about this opportunity!"
-    emotion, confidence = emotion_analyzer.get_emotion(text)
-    print(f"Detected Emotion: {emotion}, Confidence: {confidence:.2f}")
+    essential_text_in_images_matrix = [
+        [
+            "Red Line: Holy Land of Mariejoa",
+            "vanished...?",
+            "Yeah. That's what happened.",
+            "I don't mean just like a figure of speech, either. He literally vanished into thin air!!",
+            "Fuffuffu... it sure took me by surprise, I'll tell you that. Does the Kage Kage no mi have that kind of power?",
+            "This is no joking matter!!!",
+        ],
+        [
+            "Ahh, don't worry... He was half-dead already. There was no saving him, no matter where he went.",
+            "Well... unless he managed to resurrect himself as a zombie, of course... Fuffuffuffuffuffuffuffuffuffuffuffuffuffuffuffuffuffuffu! Hey, it serves him right.",
+            "And you call this doing your job, do you...?!!!",
+        ],
+    ]
+
+    result = emotion_analyzer.get_emotions_from_text_matrix(
+        essential_text_in_images_matrix
+    )
+    pprint(result)
