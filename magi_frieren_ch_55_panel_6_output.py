ae7c309984abb91f017236b2d7ac98829559fc40
Carlos Martinez <curlosmart@gmail.com>
2025-01-26 07:43:01 -0500

    Wrote some notes on the "text_character_associations" and "text_tail_associations" properties from the Magi data output after analyzing some panels from Frieren Ch. 55.

--- magi_frieren_ch_55_panel_6_output.py
+++ magi_frieren_ch_55_panel_6_output.py
@@ -30,7 +30,22 @@ magi_frieren_ch_55_panel_6_output = [
             [914.5801391601562, 617.2416381835938, 1090.65380859375, 757.5996704101562],
             [1681.2462158203125, 551.6893920898438, 1845.33740234375, 675.193115234375],
         ],
+        """
+        "text_character_associations" is pretty much the same thing as "text_tail_associations" but it uses the "characters" array instead of the tails array.
+        - [0, 0] = texts[0] is associated with characters[0]
+        - [1, 0] = texts[1] is associated with characters[0]
+        - [2, 0] = texts[2] is associated with characters[0]
+        - So, for this panel, it's one character (characters[0]) associated with all of the text. This makes sense as Methode, the name of the character in this panel, is the only one talking.
+
+        Other Notes:
+        - I'll have to run some more tests but from a quick test, it doesn't seem like Magi is currently associating characters from previous panels/images. So, if you have a panel where Goku is talking to Vegeta, then Goku is associated with ID = 0 and Vegeta is associated with ID = 1 but only for that page. So, if you go to a second page where Goku is instead talking to Piccolo, then Goku will be ID = 0 but Piccolo could in theory be ID = 1 so now 2 characters have had ID = 1 (Vegeta, Piccolo). So, this might make it difficult, if not impossible to associate characters with unique IDs and use unique TTS voices.
+        - However, it still might be possible if I train Magi using an array of passed in images so all hope is not lost yet.
+        """
         "text_character_associations": [[0, 0], [1, 0], [2, 0]],
+        # "text_tail_associations" is seemingly associating the different pieces of text from the "ocr" property to the "tails" in the speech bubbles. It looks like it's going in the order of text-index first and then tail-index.
+        # - [0, 1] means that texts[0] is associated with tails[1].
+        # - [1, 0] and [2, 0] means that texts[1] and texts[2] are both part of the same tail, tails[0].
+        # - Also, for some reason, it appears that tails are stored from left to right whereas text is stored from right-to-left so keep that in mind. This is not an issue for the text but rather that if you have text[0], it probably won't always be associated with the first index but rather the last index because tails are stored in the opposite direction: left-to-right. This is just a theory though from this one image. Will need to investigate with a larger sample size.
         "text_tail_associations": [[0, 1], [1, 0], [2, 0]],
         "character_cluster_labels": [0],
         "is_essential_text": [True, True, True],
